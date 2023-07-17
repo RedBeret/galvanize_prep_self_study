89694a12398107435d872156feb249217a7b1e2c
Steven Espi <espisteven5@gmail.com>
2023-07-16 22:30:24 -0700

    Added functions to find divisors using while and for loops (Day 9)
    In this commit, I added two functions to the codebase that compute and return a list of divisors for a given positive number. The first function, divisors_list_while, uses a while loop to iteratively check each number from 1 to the input number n and adds it to a list if it is a divisor. The loop continues until it reaches n. The second function, divisors_list_for, accomplishes the same task but utilizes a for loop to iterate through the range from 1 to n directly.
    
    Additionally, I included a test in the form of a for loop to check the functionality of both functions for numbers ranging from 0 to 19. The results of the tests are printed for each number, showing the corresponding divisors. These new additions should improve the functionality and usability of the codebase when working with divisors of positive numbers.

--- day_09_lib.py
+++ day_09_lib.py
@@ -1,38 +1,22 @@
-'''
-* Write a function that computes and returns a list of all of the divisors of some positive number. Use a while loop. Things to think about:
-    * How do you determine if a single number is a divisor of another?
-    * What is your stopping condition?
-    * BONUS: rewrite this with a for loop
-
-    input: n <int>: a positive integer
-
-    output: <list <int> >: representing all the divisor of n
-
-
-   def divisors_list(n):
-    pass
-for i in range(20):
-    print(i, divisors_list(i) ) 
-'''
-'''
-def carl_the_divisor_finder(num):
-  return_list = []
-  for each in range(1, int((num/2)+1)):
-    if num % each == 0:
-      return_list.append(each)
-  return_list.append(num)
-  return return_list
-print(carl_the_divisor_finder(10)) #[1,2,5,10]
-print(carl_the_divisor_finder(15)) #[1,2,5,15]
-'''
-'''
-def divisors_list(n):
+# Function using a while loop to find divisors of a positive number
+def divisors_list_while(n):
     lst = []
-    for i in range(n+1):
+    i = 1
+    while i <= n:
+        if n % i == 0:
+            lst.append(i)
+        i += 1
+    return lst
+
+# Function using a for loop to find divisors of a positive number
+def divisors_list_for(n):
+    lst = []
+    for i in range(1, n + 1):
         if n % i == 0:
             lst.append(i)
     return lst
 
+# Test the functions for numbers from 0 to 19
 for i in range(20):
-    print(i, divisors_list(i))
-'''
+    print(i, divisors_list_while(i))
+    print(i, divisors_list_for(i))
