731af8edf8e5a64024afb6d120ae8c6056ee26a2
Steven Espi <espisteven5@gmail.com>
2023-07-16 22:37:04 -0700

    Comparisons and Conditionals (Day 5)
    In this commit, I've added code that includes functions to check if a number is equal to either 5 or 3, to determine if a number is divisible by specific divisors, and to apply different operations based on the type of input. The functions demonstrate the use of comparisons and conditionals to perform different operations based on the given conditions. The code provides flexibility for handling various data types and conditions, enhancing the overall functionality of the program.

--- Control_flow.py
+++ Control_flow.py
@@ -1,13 +1,14 @@
-#def five_or_three(num):
-#    if num == 5 or num == 3:
-#        return True
-#    return False
-#print(five_or_three(3))
+# Day 5: Comparisons and Conditionals
 
-########################################################
+# Function to check if a number is equal to either 5 or 3
+def five_or_three(num):
+    return num == 5 or num == 3
 
-'''def is_divisible_by(num, divisor1, divisor2):
-    if num % divisor1 ==0 and num % divisor2 == 0:
+print(five_or_three(3))  # True
+
+# Function to check if a number is divisible by the provided divisors
+def is_divisible_by(num, divisor1, divisor2):
+    if num % divisor1 == 0 and num % divisor2 == 0:
         return f'This number is divisible by {divisor1} and {divisor2}.'
     elif num % divisor1 == 0:
         return f'This number is divisible by {divisor1}.'
@@ -15,29 +16,20 @@
         return f'This number is divisible by {divisor2}.'
     else:
         return f'This number is not divisible by either {divisor1} or {divisor2}.'
-    
-    
- # Prints 'This number is divisible by 3.'
-print(is_divisible_by(9, 3, 2))
 
-# Prints 'This number is divisible by 3.'
-print(is_divisible_by(12, 5, 3))
+print(is_divisible_by(9, 3, 2))  # 'This number is divisible by 3.'
+print(is_divisible_by(12, 5, 3))  # 'This number is divisible by 3.'
+print(is_divisible_by(12, 3, 4))  # 'This number is divisible by 3 and 4.'
+print(is_divisible_by(12, 5, 7))  # 'This number is not divisible by either 5 or 7.'
 
-# Prints 'This number is divisible by 3 and 4.'
-print(is_divisible_by(12, 3, 4))
-
-# Prints 'This number is not divisible by either 5 or 7.'
-print(is_divisible_by(12, 5, 7))'''
-
-####################################################
-
-'''def depends_on_the_type(obj):
+# Function to apply different operations based on the type of input
+def depends_on_the_type(obj):
     if type(obj) == int:
         if obj == 0:
             return 'Zero'
         elif obj % 2 == 0:
             return obj**2
-        elif obj % 2 ==1:
+        elif obj % 2 == 1:
             return obj**3
     elif type(obj) == float:
         return obj * 1.5
@@ -48,110 +40,10 @@ print(is_divisible_by(12, 5, 7))'''
     else:
         return None
 
-print (depends_on_the_type(0))
-print (depends_on_the_type(2))
-print (depends_on_the_type(3))
-print (depends_on_the_type(0.5))
-print (depends_on_the_type('hello'))
-print (depends_on_the_type(False))
-print (depends_on_the_type(None))'''
-
-#####################################################
-
-'''
-# comparing strings
-if 5 == 5:
-    # This will print `True`
-    print("hello there" == "hello there") 
-
-if 5 == 5:
-    # This will print `False`
-    print("hello" == "hi")
-    
-# strings can be said to be equal if they contain exactly the same content, as shown above
-
-# comparing numbers
-if 5 == 5:
-    # This will print `True`
-    print(5 == 5) 
-
-if 5 == 5:
-    # This will print `False`
-    print(5 != 5) '''
-
-#####################################################
-'''def is_old_enough_to_drink(age):
-    if age > 20:
-        return (True)
-    else:
-        return (False)
-
-output = is_old_enough_to_drink(22)
-print(output) # --> True'''
-#####################################################
-
-'''
-def is_old_enough_to_drive(age):
-    if age > 15:
-        return (True)
-    else:
-        return (False)
-
-output = is_old_enough_to_drive(15)
-print(output) # --> False
-'''
-
-#####################################################
-'''
-def is_old_enough_to_vote(age):
-    if age > 17:
-        return (True)
-    else:
-        return (False)
-
-output = is_old_enough_to_vote(22)
-print(output) # --> False'''
-####################################################
-'''def is_old_enough_to_drink_and_drive(age):
-    return (False)
-'''
-####################################################
-'''
-def special_or(expression1, expression2):
-    if not expression1 == True and not expression2 == True :
-        return False
-    return True
-
-output = special_or(True, False)
-print(output) # --> True
-'''
-####################################################
-'''
-def is_either_even_or_are_both_7(num1, num2):
-    if num1 % 2 == 0 or num2 % 2 == 0:
-        return True
-    elif num1 == 7 and num2 == 7:
-        return True
-    return False
-        
-output = is_either_even_or_are_both_7(3, 7)
-print(output) # --> False
-
-output = is_either_even_or_are_both_7(2, 3)
-print(output) # --> True
-'''
-####################################################
-'''
-def is_either_even_and_less_than_9(num1, num2):
-    if not num1 < 9 or not num2 < 9:
-        return False
-    elif not num1 % 2 == 0 and not num2 % 2 == 0:
-        return False
-    return True
-        
-output = is_either_even_and_less_than_9(2, 4)
-print(output) # --> True
-
-output = is_either_even_and_less_than_9(72, 2)
-print(output) # --> False
-'''
+print(depends_on_the_type(0))  # 'Zero'
+print(depends_on_the_type(2))  # 4
+print(depends_on_the_type(3))  # 27
+print(depends_on_the_type(0.5))  # 0.75
+print(depends_on_the_type('hello'))  # 'hellohello'
+print(depends_on_the_type(False))  # True
+print(depends_on_the_type(None))  # None
